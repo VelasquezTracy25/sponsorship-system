697cf64064a21e6f82cc037f4163f32a3b00a79e
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-11-25 00:00:25 -0600

    checked for edge cases

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# sponsorship-system
+# Simple Sponsorship System
+
+This system will accept a form submission for new leagues and write them to a MySQL database.
+
+It will also submit a form to sponsor leagues, taking in coordinates, a radius, and a budget amount to calculate the max amount of teams an organization can sponsor.
+
+## Installation
+
+This project uses Spring Boot Framework to produce frontend forms on http://localhost:8080/.
+
--- src/main/java/com/madeline/sponsorshipsystem/controllers/FormController.java
+++ src/main/java/com/madeline/sponsorshipsystem/controllers/FormController.java
@@ -32,34 +32,38 @@ public class FormController {
     }
 
     @PostMapping("/find-leagues")
-    public String showLeagues( @RequestParam(name = "coords") String coords,
-                               @RequestParam(name = "radius") int radius,
-                               @RequestParam(name = "budget") int budget,
-                               Model model){
-    List<League> acceptedLeagues = findLeagues(coords, radius, budget);
+    public String sponsorLeagues(@RequestParam(name = "coords") String coords,
+                                 @RequestParam(name = "radius") int radius,
+                                 @RequestParam(name = "budget") int budget) {
+        List<League> acceptedLeagues = findLeagues(coords, radius, budget);
 
-    int totalSpend = 0;
+        int totalSpend = 0;
 
-    for (League acceptedLeague: acceptedLeagues){
-        System.out.println("Name:" + acceptedLeague.getName());
-        System.out.println("Price:" + acceptedLeague.getPrice());
-        totalSpend = totalSpend + acceptedLeague.getPrice();
+        if (acceptedLeagues.size() <= 0) {
+            System.out.println("Your budget of $" + budget + " cannot be used to sponsor a league.");
+            System.out.println("--------------------------------------");
+        } else {
+            System.out.println("--------------------------------------");
+            for (League acceptedLeague : acceptedLeagues) {
+                System.out.println("Name:" + acceptedLeague.getName());
+                System.out.println("Price:" + acceptedLeague.getPrice());
+                totalSpend = totalSpend + acceptedLeague.getPrice();
+            }
+            System.out.println("Total Budget Used: " + totalSpend);
+            System.out.println("Total Budget Remaining: " + (budget - totalSpend));
+            System.out.println("--------------------------------------");
+        }
+        return "redirect:/";
     }
-        System.out.println("Total Budget Used: " + totalSpend);
-        System.out.println("Total Budget Remaining: " + (budget - totalSpend));
-    return "redirect:/";
-    }
-
 
     public List<League> findLeagues(String coords, int radius, int budget) {
 
         List<League> leagues = leagueRepo.findAll(Sort.by(Sort.Direction.ASC, "price"));
         List<League> acceptedLeagues = new ArrayList<>();
 
-
         for (League league : leagues) {
             if (budget - league.getPrice() <= 0)
-            return acceptedLeagues;
+                return acceptedLeagues;
             else if (league.getPrice() <= budget) {
                 budget = budget - league.getPrice();
                 acceptedLeagues.add(league);
